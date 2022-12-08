2c05c1935cee550a93bd1eae879c5266aac449e7
Fabio Furlan <85201873+fabio-furlan@users.noreply.github.com>
2022-12-08 14:02:20 -0300

    Add Exercício 10

---
+++ livro/Exercicio10.java
@@ -0,0 +1,25 @@
+package br.com.livro;
+
+public class Exercicio10 {
+
+	public static void main(String[] args) {
+
+		String A = "1 - Levar a grama e o leão";
+		String B = "2 - Voltar com o leão";
+		String C = "3 - Deixar o leão";
+		String D = "4 - Levar a cabra";
+		String E = "5 - Deixar a cabra";
+		String F = "6 - Voltar com a grama";
+		String G = "7 - Levar o leão e a grama";
+
+		System.out.println("Algoritmo para levar o leão,cabra e grama para outro lado do rio");
+
+		System.out.println("-----------------------------------------------------------------");
+
+		System.out.printf("%s \n%s \n%s \n%s \n%s \n%s \n%s", A, B, C, D, E, F, G);
+
+		System.out.println();
+		System.out.println("------------------------------------------------------------------");
+	}
+
+}
